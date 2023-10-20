7b9a23eef749533a24d45fc905be8b412d3b54bb
huypd <huypd@10.12.22.45>
2023-10-20 14:25:35 +0700

    check Data Format

---
+++ src/main/java/main/J1SP0064.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+import controller.ValidateProgram;
+
+/**
+ *
+ * @author huypd
+ */
+public class J1SP0064 {
+
+    public static void main(String[] args) {
+        new ValidateProgram().run();
+    }
+}
